3ec580a4f0bb71889126adaa13930d88d808d56c
Cocomax1904 <cocomax1904@gmail.com>
2025-05-04 01:13:53 +0200

    Mise à jour du manifeste et des méta pour PWA iOS

--- assets/js/main.js
+++ assets/js/main.js
@@ -38,7 +38,7 @@ document.addEventListener("DOMContentLoaded", () => {
 // === Point 5 PWA: enregistrement du Service Worker ===
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/service-worker.js')
+    navigator.serviceWorker.register('service-worker.js')
       .then(reg => console.log('✅ Service Worker enregistré avec succès :', reg.scope))
       .catch(err => console.error('❌ Échec de l’enregistrement du SW :', err));
   });
